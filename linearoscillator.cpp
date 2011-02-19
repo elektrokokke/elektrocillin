4ff8acba1ee60e14307c1f7fb623e12b21d6cd61
elektrokokke <elektrokokke@gmail.com>
2011-02-19 01:48:44 +0100

    Another (failed) attempt at removing alias from LinearOscillator

--- linearoscillator.cpp
+++ linearoscillator.cpp
@@ -7,39 +7,23 @@ LinearOscillator::LinearOscillator(double frequencyModulationIntensity, double s
     Oscillator(frequencyModulationIntensity, sampleRate, additionalInputPortNames),
     interpolator(QVector<double>(1), QVector<double>(1)),
     integral(interpolator),
-    sincIntegralValues(sincWindowSize * 2, 0)
+    sineIntegral(QVector<double>(0), QVector<double>(0), QVector<double>(0))
 {
     interpolator.getX()[0] = 0;
     interpolator.getY()[0] = -1;
     interpolator.getX().append(2 * M_PI);
     interpolator.getY().append(1);
     integral = LinearIntegralInterpolator(interpolator);
-    for (int i = 0; i < sincWindowSize * 2; i++) {
-        integralValuesQueue.append(0);
-    }
-    // compute the integral of the sinc function between multiples of PI:
-    double previousSincIntegralValue = 0;
-    double sum = 0;
-    for (int i = 0; i < sincWindowSize; i++) {
-        std::complex<double> cs;
-        cisi(M_PI * (i + 1), cs);
-        double sincIntegralValue = cs.imag();
-        sincIntegralValues[sincWindowSize + i] = sincIntegralValues[sincWindowSize - i - 1] = sincIntegralValue - previousSincIntegralValue;
-        qDebug() << sincIntegralValues[sincWindowSize + i];
-        sum += sincIntegralValues[sincWindowSize + i] * 2;
-        previousSincIntegralValue = sincIntegralValue;
-    }
-    for (int i = 0; i < sincWindowSize * 2; i++) {
-        sincIntegralValues[i] /= sum;
-    }
-    qDebug() << sum;
+    initializeSineIntegral(sincWindowSize);
 }
 
-LinearOscillator::LinearOscillator(const LinearInterpolator &interpolator_, double frequencyModulationIntensity, double sampleRate) :
+LinearOscillator::LinearOscillator(const LinearInterpolator &interpolator_, double frequencyModulationIntensity, double sampleRate, int sincWindowSize) :
     Oscillator(frequencyModulationIntensity, sampleRate),
     interpolator(interpolator_),
-    integral(interpolator_)
+    integral(interpolator_),
+    sineIntegral(QVector<double>(0), QVector<double>(0), QVector<double>(0))
 {
+    initializeSineIntegral(sincWindowSize);
 }
 
 const LinearInterpolator & LinearOscillator::getLinearInterpolator() const
@@ -64,23 +48,67 @@ double LinearOscillator::valueAtPhase(double phase, double previousPhase)
     integralValuesQueue.dequeue();
     // calculate the news value and add it to the queue:
     if (phase == previousPhase) {
-        integralValuesQueue.enqueue(0);
+        QList<QPair<double, double> > list;
+        list.append(QPair<double, double>(0, 1));
+        integralValuesQueue.enqueue(list);
     } else {
-        double previousIntegralValue = integral.evaluate(previousPhase);
-        // compare current phase with previous phase:
-        if (phase < previousPhase) {
-            // phase has wrapped around, adjust the previous integral level accordingly:
-            previousIntegralValue -= integral.interpolate(integral.getX().size() - 2, integral.getX().back());
-            previousPhase -= 2 * M_PI;
+        QList<QPair<double, double> > list;
+        double phaseDifference = phase - previousPhase;
+        int previousIndex, index;
+        double previousIntegralValue = integral.evaluate(previousPhase, &previousIndex);
+        double integralValue = integral.evaluate(phase, &index);
+        for (int i = previousIndex; i != index; ) {
+            double intermediatePhase = integral.getX()[i + 1];
+            if (intermediatePhase != previousPhase) {
+                double intermediateIntegralValue = integral.interpolate(i, intermediatePhase);
+                list.append(QPair<double, double>((intermediateIntegralValue - previousIntegralValue) / (intermediatePhase - previousPhase), (intermediatePhase - previousPhase) / phaseDifference));
+                previousPhase = intermediatePhase;
+                previousIntegralValue = intermediateIntegralValue;
+            }
+            i++;
+            if (i == integral.getX().size() - 1) {
+                i = 0;
+                previousPhase = integral.getX()[0];
+                previousIntegralValue = integral.interpolate(0, previousPhase);
+            }
         }
-        double integralValue = integral.evaluate(phase);
-        // integral difference / phase difference is the oscillator output:
-        integralValuesQueue.enqueue((integralValue - previousIntegralValue) / (phase - previousPhase));
+        if (phase != previousPhase) {
+            list.append(QPair<double, double>((integralValue - previousIntegralValue) / (phase - previousPhase), (phase - previousPhase) / phaseDifference));
+        }
+        integralValuesQueue.enqueue(list);
     }
     // now band-limit the signal using the sinc function:
     double output = 0;
-    for (int i = 0; i < sincIntegralValues.size(); i++) {
-        output += sincIntegralValues[i] * integralValuesQueue[i];
+    for (int i = 0; i < integralValuesQueue.size(); i++) {
+        double x = sineIntegral.getX()[i];
+        double previousSincIntegralValue = sineIntegral.evaluate(x);
+        QList<QPair<double, double> > &list = integralValuesQueue[i];
+        for (int j = 0; j < list.size(); j++) {
+            double oscillatorIntegralValue = list[j].first;
+            x += list[j].second;
+            double sincIntegralValue = sineIntegral.evaluate(x);
+            output += oscillatorIntegralValue * (sincIntegralValue - previousSincIntegralValue);
+            previousSincIntegralValue = sincIntegralValue;
+        }
     }
     return output;
+}
+
+void LinearOscillator::initializeSineIntegral(int sincWindowSize)
+{
+    // create a cubic spline that approximates the cardinal sine integral:
+    QVector<double> xx, yy;
+    for (int i = -sincWindowSize; i <= sincWindowSize; i++) {
+        double x = M_PI * i;
+        std::complex<double> cs;
+        cisi(x, cs);
+        xx.append(i);
+        yy.append(cs.imag() / M_PI);
+        if (i < sincWindowSize) {
+            QList<QPair<double, double> > list;
+            list.append(QPair<double, double>(0, 1));
+            integralValuesQueue.append(list);
+        }
+    }
+    sineIntegral = CubicSplineInterpolator(xx, yy);
 }
--- linearoscillator.h
+++ linearoscillator.h
@@ -3,17 +3,18 @@
 
 #include "oscillator.h"
 #include "linearintegralinterpolator.h"
+#include "cubicsplineinterpolator.h"
 #include <QQueue>
-#include <QVector>
+#include <QPair>
 
 class LinearOscillator : public Oscillator
 {
 public:
-    LinearOscillator(double frequencyModulationIntensity = 2.0/12.0, double sampleRate = 44100, const QStringList &additionalInputPortNames = QStringList(), int sincWindowSize = 16);
+    LinearOscillator(double frequencyModulationIntensity = 2.0/12.0, double sampleRate = 44100, const QStringList &additionalInputPortNames = QStringList(), int sincWindowSize = 8);
     /**
       @param interpolator has to span the interval [0..2 * pi]
       */
-    LinearOscillator(const LinearInterpolator &interpolator, double frequencyModulationIntensity = 2.0/12.0, double sampleRate = 44100);
+    LinearOscillator(const LinearInterpolator &interpolator, double frequencyModulationIntensity = 2.0/12.0, double sampleRate = 44100, int sincWindowSize = 8);
 
     const LinearInterpolator & getLinearInterpolator() const;
     void setLinearInterpolator(const LinearInterpolator &interpolator);
@@ -25,8 +26,10 @@ protected:
 private:
     LinearInterpolator interpolator;
     LinearIntegralInterpolator integral;
-    QQueue<double> integralValuesQueue;
-    QVector<double> sincIntegralValues;
+    CubicSplineInterpolator sineIntegral;
+    QQueue<QList<QPair<double, double> > > integralValuesQueue;
+
+    void initializeSineIntegral(int sincWindowSize);
 };
 
 #endif // LINEAROSCILLATOR_H
