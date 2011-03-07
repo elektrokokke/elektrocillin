f02c684dca0b946a140713da6b964967880c470f
elektrokokke <elektrokokke@gmail.com>
2011-03-07 18:00:59 +0100

    Fixed mousewheel zooming to keep the position under the cursor fixed

--- wheelzoominggraphicsview.cpp
+++ wheelzoominggraphicsview.cpp
@@ -16,12 +16,14 @@ void WheelZoomingGraphicsView::setScene(QGraphicsScene *scene)
 void WheelZoomingGraphicsView::wheelEvent(QWheelEvent *event)
 {
     // get the scene position of the mouse position:
-    QPointF pos = mapToScene(event->pos());
-    centerOn(pos);
+    QPointF scenePosUnderMouseBefore = mapToScene(event->pos());
     if (event->delta() > 0) {
         scale(1.25, 1.25);
     } else if (event->delta() < 0) {
         scale(0.8, 0.8);
     }
+    QPointF scenePosUnderMouseAfter = mapToScene(event->pos());
+    QPointF scenePosUnderCenterAfter = mapToScene(QPoint(viewport()->rect().width() / 2, viewport()->rect().height() / 2));
+    centerOn(scenePosUnderCenterAfter + scenePosUnderMouseBefore - scenePosUnderMouseAfter);
 }
 
